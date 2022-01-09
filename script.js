02a96a33dadb3053f482bbf60333217aeb1c130e
Evilyn Araújo <77305238+Luaon@users.noreply.github.com>
2022-01-09 19:45:46 -0300

    09 de Janeiro de 2022 07:45PM

---
+++ script.js
@@ -0,0 +1,11 @@
+const altura = document.getElementById('altura');
+const peso = document.getElementById.apply('pes');
+const calcular = document.getElementById('calcular');
+const result = "";
+
+function  imc (){
+    alert('test');
+
+
+}
+calcular.addEventListener('click', imc);
