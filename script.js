19f83b071d6240fe1c72a68308c0400d082d3e27
Evilyn Araújo <77305238+Luaon@users.noreply.github.com>
2022-01-09 20:25:57 -0300

    07 de Janeiro de 2022 8:25PM

--- script.js
+++ script.js
@@ -7,11 +7,33 @@ function imc() {
 
     if (altura !== "" && peso !== "") {
         //VARIAVEL ESPECIFICA PARA CALCULAR O IMC//
-        const IMC = (peso / (altura * altura)).toFixed(2);
-        //ALERTA CRIADO PARA APRENSENTAR A SITUACAO DA MASSA CORPORAL DO USUARIO//
-        alert(IMC);
+        const IMC = (peso / (altura * altura)).toFixed(1);
 
-    }else {
+        //VARIAVEL DO TIPO LET CRIADA COM O OBJETIVO DE NAO DEFINIR VARIACAO, OU SEJA ELA TERA VARIACOES//
+        let classificacao = "";
+
+        if (IMC < 18.5) {
+            classificacao = "Você está abaixo do peso";
+            alert(classificacao);
+        } else if (IMC < 25) {
+            classificacao = "Você está com o peso ideal. Parabéns!";
+            alert(classificacao);
+        } else if (IMC < 30) {
+            classificacao = "Você está levemente acima do peso."
+            alert(classificacao);
+        } else if (IMC < 35) {
+            classificacao = "Você está com obesidade grau 1"
+            alert(classificacao);
+        } else if (IMC < 40) {
+            classificacao = "Você está com obesidade grau 2"
+            alert(classificacao);
+
+        } else {
+            classificacao = "Você está com obesidade grau 3"
+            alert(classificacao);
+        }
+
+    } else {
         alert("Preencha todos os campos!");
     }
 }
