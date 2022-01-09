dd4f87a09e21b390122c44163bd58a7b9fad459a
Evilyn Araújo <77305238+Luaon@users.noreply.github.com>
2022-01-09 20:11:46 -0300

    09 de Janeiro de 2022 8:11PM

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const altura = document.getElementById('altura');
-const peso = document.getElementById.apply('pes');
 const calcular = document.getElementById('calcular');
-const result = "";
 
-function  imc (){
-    alert('test');
+//FUNCAO DE CALCULOS E APRESENTACAO DE VARIAVEIS DE ALTURA E PESO//
+function imc() {
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
 
+    if (altura !== "" && peso !== "") {
+        //VARIAVEL ESPECIFICA PARA CALCULAR O IMC//
+        const IMC = (peso / (altura * altura)).toFixed(2);
+        //ALERTA CRIADO PARA APRENSENTAR A SITUACAO DA MASSA CORPORAL DO USUARIO//
+        alert(IMC);
 
+    }else {
+        alert("Preencha todos os campos!");
+    }
 }
+
+//PROPRIEDADE PARA CLICK NO BOTAO//
 calcular.addEventListener('click', imc);
